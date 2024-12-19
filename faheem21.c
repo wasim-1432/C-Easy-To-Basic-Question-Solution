4de9cbaaf1c4f40e3dd3c99652e523201c985fd2
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-19 23:22:58 +0530

    Calculate average of three integer numbers

---
+++ faheem21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter any three Integers\n");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("Average is=%f",(a+b+c)/3.0);
+    printf("\n");
+    return 0;
+}
