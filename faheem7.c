d8c209385a2d20d3596514783286adbc2cd42341
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-19 23:14:56 +0530

    Calculate area of Rectangle

---
+++ faheem7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int l,w;
+    printf("Enter the length and width of the rectangle\n");
+    scanf("%d%d",&l,&w);
+    printf("Area of the reactangle is=%d",l*w);
+    printf("\n");
+    return 0;
+}
